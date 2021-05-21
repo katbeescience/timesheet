f6623f23ff27deb7c52b9bd88402e8c6a390ddfd
Kathryn Busby <mkbusby@email.arizona.edu>
2021-05-21 13:16:27 -0700

    Apply while loop to repeat query.

--- timesheet.R
+++ timesheet.R
@@ -14,13 +14,25 @@ library(chron)
 # Calculate time difference between start and end time. We'll make a function.
 
 convert.time <- function(){
-start <- readline(prompt="Enter start time: ")
-end <- readline(prompt="Enter end time: ")
+  startinput <- "b"
+  start <- NULL
+  end <- NULL
+  
+  while (startinput != "d") {
+    startinput <- readline(prompt="Enter start time: ")
+    endinput <- readline(prompt="Enter end time: ")
+    start <- c(start, startinput)
+    end <- c(end, endinput)
+  }
+  
 starttime <- strptime(start, "%I:%M %p")
 endtime <- strptime(end, "%I:%M %p")
 dur.table <- data.frame(starttime, endtime)
 dur.table$diff <- dur.table$endtime - dur.table$starttime
-sum <- sum(dur.table$diff)
+sum <- sum(dur.table$diff[1:(length(dur.table$diff)-1)])
 return(sum)
 }
+
+convert.time()
+
 
