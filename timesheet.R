cf82ef8ce9550af3a0c7a343375f1178dcbcdb8d
Kathryn Busby <mkbusby@email.arizona.edu>
2021-05-21 12:56:00 -0700

    Add participant input.

--- timesheet.R
+++ timesheet.R
@@ -13,7 +13,9 @@ library(chron)
 
 # Calculate time difference between start and end time. We'll make a function.
 
-convert.time <- function(start, end){
+convert.time <- function(){
+start <- readline(prompt="Enter start time: ")
+end <- readline(prompt="Enter end time: ")
 starttime <- strptime(start, "%I:%M %p")
 endtime <- strptime(end, "%I:%M %p")
 dur.table <- data.frame(starttime, endtime)
